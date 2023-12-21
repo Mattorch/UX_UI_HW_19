1fb5111fb3997ce52edfd28f6e53059bfa56f0f9
Mattorch <136012596+Mattorch@users.noreply.github.com>
2023-12-21 16:27:07 -0700

    Javascript added

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // Change background color on hover using jQuery
+    $('.inline-block a').hover(
+        function () {
+            $(this).css('background-color', '#FFB076');
+        },
+        function () {
+            $(this).css('background-color', '');
+        }
+    );
+});
